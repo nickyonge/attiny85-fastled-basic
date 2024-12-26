4d8838d2d54de954628c7ba66655062ae050d093
Nick Yonge <nick@curiobot.com>
2024-12-26 14:23:28 -0800

    import LEDs setup

--- src/leds.cpp
+++ src/leds.cpp
@@ -4,10 +4,41 @@
 
 CRGB leds[FASTLED_LEDS];
 
+#ifdef ONLY_UPDATE_LEDS_ON_CHANGE
+CRGB lastRGB;
+#endif
+
+#ifdef DEBUG_RAINBOW_BLACKOUT_PATTERN
+byte rbpHue = 0;
+bool rbpBlackout = false;
+#endif // DEBUG_RAINBOW_BLACKOUT_PATTERN
+
 void setupLEDs()
 {
     // prep LED strip via FastLED
     FastLED.addLeds<WS2812B, PIN_LED_DATA, GRB>(leds, FASTLED_LEDS);
+
+    // prep initial RGB value
+#ifdef ONLY_UPDATE_LEDS_ON_CHANGE
+    lastRGB = CRGB::White;
+#endif
+
+    // clear any residual data in the LED strip
+    FastLED.clearData();
+
+#ifdef SET_LEDS_RAINBOW_ON_START
+    byte rainbowMultiplier = 255 / FASTLED_LEDS;
+#endif
+
+    for (int i = 0; i < FASTLED_LEDS; i++)
+    {
+#ifdef SET_LEDS_RAINBOW_ON_START
+        leds[i] = CRGB(CHSV(i * rainbowMultiplier, 255, 255));
+#else
+        leds[i] = CRGB::Black;
+#endif
+    }
+    FastLED.show();
 
 }
 void loopLEDs()
